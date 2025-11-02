07e21947515533663f638f4f81fc593b8f98b9a9
CodeClashers89 <clasherscode6@gmail.com>
2025-11-02 12:35:05 +0530

    All pages app

--- app.py
+++ app.py
@@ -6,5 +6,26 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/Mobile')
+def mobile():
+    return render_template('mobile.html')
+
+@app.route('/Laptop')
+def laptop():
+    return render_template('laptop.html')
+
+@app.route('/TV')
+def tv():
+    return render_template('tv.html')
+
+@app.route('/Sound System')
+def ss():
+    return render_template('ss.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
